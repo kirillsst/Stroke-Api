dbd187f0e10bd770cc7e2911f59723d4745c273a
kirillsst <jewaor@gmail.com>
2025-08-07 16:53:14 +0200

    presque fini stats API

--- stroke_api/api.py
+++ stroke_api/api.py
@@ -1,21 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from stroke_api import filters
 
 router = APIRouter()
+
 
 @router.get("/")
 def read_root():
     return {"message": "Bienvenue sur l'API Stroke Prediction !"}
 
-# TODO décommenter et compléter
-#@router.get("/patients/")
-#def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
-    #filtered_df = 
-    #return filtered_df.to_dict(orient="records")
+# # TODO décommenter et compléter
+@router.get("/patients/")
+def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
+    filtered_df = filters.filter_patient(gender=gender, stroke=stroke, max_age=max_age)
+    return filtered_df
 
 # TODO décommenter et compléter
-#@router.get("/patients/{patient_id}")
-    # Gérer le cas où l'id de patient passé en paramètre n'existe pas
+@router.get("/patients/{patient_id}")
+def get_patient_by_id(patient_id: int):
+    patient = filters.get_id(patient_id)
 
+    if patient is None:
+        raise HTTPException(status_code=404, detail="Patient not found")
 
+    return patient
 # TODO Ajout de la route stats
+
+@router.get("/stats/")
+def get_stats(stats):
+    df = filters.statistic()
+
+    stats = {
+        "total_patients": len(df),
+        "stroke_count": df['stroke'].sum(),
+        "average_age": round(df['age'].mean(), 2),
+    }
+    return stats
